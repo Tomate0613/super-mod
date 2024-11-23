d84cd39e9f0667eb2a48ace24ce194cdc60e3171
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-11-23 17:27:17 +0100

    Change help message

--- src/main/resources/data/super_mod/scripts/help.lua
+++ src/main/resources/data/super_mod/scripts/help.lua
@@ -1,5 +1,5 @@
 print("HELP")
-print("Use '> ls' to list available programs")
-print("Drag and drop a file onto the block to put it in the filesystem")
-print("Use '> upload' to sync changes made to the filesystem to other players")
+print("")
+print("Please refer to the docs:")
+print("https://github.com/Tomate0613/super-mod")
 print("")
